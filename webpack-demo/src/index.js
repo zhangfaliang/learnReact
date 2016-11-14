c054d900b8a6bace2260ef9e5461092ecb7e3181
fl.zhang@8win.com <zhangfaliang>
2016-11-14 16:08:10 +0800

    基本配置运行react

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,17 +1,17 @@
 import  React,{ Component, PropType } from  'react'
 import { render } from 'react-dom'
-
+console.log(render)
 class App extends Component{
 	constructor(props){
 		super(props);
 	}
 	render(){
 		return(
-			<h1>欢迎shiny教学</h1>
+			<h1>react</h1>
 			)
 	}
 }
 
 let root=document.getElementById('app')
 
-render(<app/>,root)
+render(<App/>,root)
