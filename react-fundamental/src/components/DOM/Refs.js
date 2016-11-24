fe45e0d295fea8460a1039e9c68cf5d8ed39086e
fl.zhang@8win.com <zhangfaliang>
2016-11-24 11:28:46 +0800

    又去的操作

--- react-fundamental/src/components/DOM/Refs.js
+++ react-fundamental/src/components/DOM/Refs.js
@@ -1,52 +1,62 @@
 import React,{ Component, PropTypes } from 'react'
 import ReactDOM ,{ findDOMNode } from 'react-dom'
 
-export default class Refs extends Component{
-	constructor(props){
-		super(props);
-		this.setState={
-			red:0,
-			green:0,
-			blue:0
-		}
-		this.handleUpdate=this.handleUpdate.bind(this);
-	}
-	handleUpdate(){
-		this.setState({
-			red:this.refs.red.value,
-
-		})
-	}
-	render(){
-		return(
-			<div>
-				<Slider 
-					ref='red'
-					update={this.handleUpdate}/>
-					<br/>
-				<Slider 
-					ref='green'
-					update={this.handleUpdate}/>
-					<br/>
-				<Slider 
-					ref='blue'
-					update={this.handleUpdate}/>
-				
-			</div>
-			)
-	}
-}
-
 class Slider extends Component{
 	constructor(props){
 		super(props)
+		
 	}
 	render(){
 		return(
 			<input type='range'
 				min='0'
 				max='255'
-			 onChane={this.props.update}/>
+			 onChange={this.props.update}/>
+			)
+	}
+
+}
+
+export default class Refs extends Component{
+	constructor(props){
+		super(props);
+		this.state={
+			red:'0',
+			blue:'0',
+			green:0
+		}
+		this.handleUpdate=this.handleUpdate.bind(this)
+	}
+	handleUpdate(e){
+		this.setState({
+			red:findDOMNode(this.refs.red).value,
+			green:findDOMNode(this.refs.green).value,
+			blue:findDOMNode(this.refs.blue).value,
+
+		})
+	//console.dir(this.refs.blue)
+	}
+	render(){
+		let { red, green, blue } = this.state
+		return( 
+			<div>
+				<Slider 
+					ref='red'
+					update={this.handleUpdate}/>
+					{red}
+										<br/>
+				<Slider 
+					ref='green'
+					update={this.handleUpdate}/>
+					{green}
+					<br/>
+				<Slider 
+					ref='blue'
+					update={this.handleUpdate}/>
+					{blue}
+				
+			</div>
 			)
 	}
 }
+
