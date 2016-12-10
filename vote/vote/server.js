208f971167a50b4fab4d8092c8d690e2c525d650
fl.zhang@8win.com <zhangfaliang>
2016-12-10 14:23:42 +0800

    调试接口ok

--- vote/vote/server.js
+++ vote/vote/server.js
@@ -7,7 +7,18 @@ var dateJson={
     {name:'pangpang',passWord:'888',id:'88',sex:'女'}
   ]
 }
-
+/*项目需求
+实现投票获奖活动，票数越高，排名越高。
+只有报名用户才可以参加投票，每人最多投5票，每一个投票者最多只能投一票给同一个人。
+报名用户既是投票者，也是被投票者。
+用户未登入时，首页橙色按钮显示我要报名状态，点击进入报名页。
+用户已登入，首页橙色按钮显示个人主页，点击进入登入者的个人主页。
+用户未登入进行投票，点击投票则提示用户先登入或者报名才可以参与投票。
+首页搜索内容可以为用户的名称或编号，如有多个相同名称，则返回所有匹配名称的用户信息。
+搜索页的用户内容也可以进行投票，投票行为和首页的投票行为一致。
+首页用户信息通过上拉加载刷新进行分页，每页为十条数据。
+个人详细页需要显示投票者信息。
+*/
 // 创建 application/x-www-form-urlencoded 编码解析
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
@@ -19,7 +30,9 @@ app.get('/', function (req, res) {
 app.get('/vote/register', function (req, res) {
    res.sendFile( __dirname + "/src/signUp/signUp.html" );
 })
-
+app.get('/vote/vote', function (req, res) {
+   res.sendFile( __dirname + "/src/rule/rule.html" );
+})
 /*
 首页：localhost:8080/vote/index
 报名页：localhost:8080/vote/register
@@ -42,14 +55,19 @@ app.post("/vote/index/info",function(req,res){
 app.post('/vote/register/data', urlencodedParser, function (req, res) {
 
    // 输出 JSON 格式
-   response = {
-    username:req.body.username, 
-    mobile: req.body.mobile ,
-    description:req.body.description,
-    gender:req.body.gender ,
-    password:req.body.password
+   console.log(req.body)
+   for(var attr in req.body ){
+    attr=JSON.parse(attr)
+      response = {
+      username:attr.username, 
+      mobile: attr.mobile ,
+      description:attr.description,
+      gender:attr.gender ,
+      password:attr.password
    };
-   res.end(JSON.stringify(response));
+   }
+   dateJson.userInfo.push(response)
+   res.end(JSON.stringify({message:'ok'}));
 })
 
 app.post('/process_post', urlencodedParser, function (req, res) {
